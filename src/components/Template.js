8e034eb1eb7511e30e92d174738d08f37c1700d3
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-06-05 16:54:46 +0530

    work in progress

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-const Template = ({title ,}) => {
+const Template = ({title , desc1 , desc2 , image , formType , isLoggedIn}) => {
+    // formType is which type of form login or signup
     return (
-        klsdfj
+        <div>
+            <div>
+                <h1>{title}</h1>
+                <p>{desc1}</p>
+                <p>{desc2}</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
     )
 }
 
